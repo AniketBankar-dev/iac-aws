e1bba6aaac503e797e515bc537395351799bd06a
AniketBankar-dev <18aniketbankar@gmail.com>
2026-03-23 14:14:04 +0530

    added user data

--- main.tf
+++ main.tf
@@ -4,5 +4,39 @@ provider "aws" {
  resource "aws_instance" "terra" {
     ami = "ami-0f5ee92e2d63afc18"
     instance_type = "t2.micro"
-    #retry run 
- }
+
+    
+  user_data = <<-EOF
+              #!/bin/bash
+
+              # Update system
+              apt update -y
+
+              # Install Docker
+              apt install docker.io -y
+              systemctl start docker
+              systemctl enable docker
+
+              # Install Apache (httpd)
+              apt install apache2 -y
+              systemctl start apache2
+              systemctl enable apache2
+
+              # Install Kubernetes tools
+              apt install -y apt-transport-https ca-certificates curl
+
+              curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+
+              cat <<EOT >> /etc/apt/sources.list.d/kubernetes.list
+              deb https://apt.kubernetes.io/ kubernetes-xenial main
+              EOT
+
+              apt update -y
+              apt install -y kubelet kubeadm kubectl
+
+              EOF
+
+  tags = {
+    Name = "DevOps-EC2"
+  }
+}
